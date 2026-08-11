6ac479aa07474e39c719b420da3b4b1864e017d1
Ellen Sesoko <sesoko.ee@gmail.com>
2026-08-11 12:36:45 +0200

    Refactor Brew model to include additional fields

--- backend/src/models/brew.js
+++ backend/src/models/brew.js
@@ -1,52 +1,50 @@
+:::writing{variant="document" id="68421"}
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../sequelize');
 
-const Brew = sequelize.define(
-'Brew',
-{
-id: {
-type: DataTypes.INTEGER,
-autoIncrement: true,
-primaryKey: true
-},
+const Brew = sequelize.define('Brew', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  beans: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  method: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  coffeeGrams: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  waterGrams: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  rating: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  tastingNotes: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  }
+}, {
+  tableName: 'brews',
+  timestamps: false
+});
 
-```
-beans: {
-  type: DataTypes.STRING,
-  allowNull: false
-},
+module.exports = Brew;
+:::
 
-method: {
-  type: DataTypes.STRING,
-  allowNull: false
-},
+### VERY IMPORTANT
 
-coffeeGrams: {
-  type: DataTypes.INTEGER,
-  allowNull: false
-},
+In VS Code, your file should look like this:
 
-waterGrams: {
-  type: DataTypes.INTEGER,
-  allowNull: false
-},
-
-rating: {
-  type: DataTypes.INTEGER,
-  allowNull: false
-},
-
-tastingNotes: {
-  type: DataTypes.TEXT,
-  allowNull: false
-}
-```
-
-},
-{
-tableName: 'brews',
-timestamps: false
-}
-);
-
+```text
+const { DataTypes } = require('sequelize');
+...
 module.exports = Brew;
